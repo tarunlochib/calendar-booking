52076380814494a68aa4e892623844809b418568
Tarun <tarunlochib99@gmail.com>
2025-07-14 22:43:53 +0530

    Initial commit: Create React App setup

---
+++ index.js
@@ -0,0 +1,17 @@
+   // index.js
+   const express = require('express');
+   const cors = require('cors');
+   require('dotenv').config();
+
+   const app = express();
+   app.use(cors());
+   app.use(express.json());
+
+   app.get('/', (req, res) => {
+     res.send('Shopify Calendar App Backend Running!');
+   });
+
+   const PORT = process.env.PORT || 3001;
+   app.listen(PORT, () => {
+     console.log(`Server running on port ${PORT}`);
+   });
